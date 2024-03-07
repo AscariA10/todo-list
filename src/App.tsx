4dde071240a08e5421fa47b89c9c444cf16e72d4
AscariA10 <109697703+AscariA10@users.noreply.github.com>
2024-03-07 11:40:11 +0200

    delete rubish

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,11 @@
-import { useEffect } from "react";
-
 import { TaskCard } from "./components/task-card/TaskCard";
 import { FilterBar } from "./components/filter-bar/FilterBar";
 
-import { useAppSelector, useAppDispatch } from "./redux/hooks";
-import { addTask, deleteTask, toggleComplete } from "./redux/tasks-slice";
-import { getTasks, getStatusFilter } from "./redux/selectors";
+import { useAppSelector } from "./redux/hooks";
 
 function App() {
    const tasks = useAppSelector(state => state.tasks);
    const filter = useAppSelector(state => state.filter.status);
-   const dispatch = useAppDispatch();
-
-   // dispatch(deleteTask());
-   // dispatch(toggleComplete());
-
-   // useEffect(() => {
-   //    dispatch(addTask("my new task for test"));
-   // });
-
-   function handleClick(): void {
-      dispatch(addTask("my new task for test"));
-   }
 
    console.log(tasks);
    console.log(filter);
